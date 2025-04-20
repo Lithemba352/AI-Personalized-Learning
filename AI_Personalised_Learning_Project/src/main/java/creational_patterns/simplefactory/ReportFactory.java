79360ef5e50d5d322d563b853940eafbee2fbd7e
Lithemba352 <nkalithemba7@gmail.com>
2025-04-20 20:07:43 +0200

    Implemented the six creational patterns

--- AI_Personalised_Learning_Project/src/main/java/creational_patterns/simplefactory/ReportFactory.java
+++
@@ -1,7 +0,0 @@
-package creational_patterns.simplefactory;
-
-public class ReportFactory {
-    public static Report createReport(String reportId, String userId) {
-        return new Report(reportId, userId);
-    }
-}
